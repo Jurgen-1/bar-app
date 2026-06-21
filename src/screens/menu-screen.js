5eabca944362ba8bd83ca206f0420442c641e83d
Jurgen Isufaj <notjuri20@gmail.com>
2026-06-21 13:05:20 +0200

    Menu List

--- src/screens/menu-screen.js
+++ src/screens/menu-screen.js
@@ -5,10 +5,51 @@ const app = document.querySelector("#app");
 
 export function renderMenuScreen () {
     renderHeader()
+    renderMenuItems()
 }
 
 function renderHeader () {
     const template = document.querySelector("#menu-header-template");
     const node = template.content.cloneNode(true);
     app.appendChild(node);
+}
+
+function renderMenuItems () {
+    const container = document.createElement("div");
+    container.classList.add("menu-items")
+
+    // const menuItem = state.menuItems[0];
+    // const rendered = renderMenuItem(menuItem);
+    // container.appendChild(rendered);
+
+    // const menuItem1 = state.menuItems[1];
+    // const rendered1 = renderMenuItem(menuItem1);
+    // container.appendChild(rendered1);
+
+    // const menuItem2 = state.menuItems[2];
+    // const rendered2 = renderMenuItem(menuItem2);
+    // container.appendChild(rendered2);
+
+    
+    state.menuItems.forEach((menuItem)=> {
+        const rendered = renderMenuItem(menuItem)
+        container.appendChild(rendered)
+    }) 
+    
+    app.appendChild(container);
+}
+
+function renderMenuItem(menuItem) {
+    const template = document.querySelector("#menu-item-template");
+    const node = template.content.cloneNode(true);
+
+    const menuItemName = node.querySelector(".menu-item-name")
+    const menuItemDescription = node.querySelector(".menu-item-description")
+    const menuItemPrice = node.querySelector(".menu-item-price")
+
+    menuItemName.innerText = menuItem.name
+    menuItemDescription.innerText = menuItem.description
+    menuItemPrice.innerText = menuItem.price
+
+    return node
 }
--- src/state.js
+++ src/state.js
@@ -1,4 +1,104 @@
 export const state = {
     currentScreen: "MENU",
-    menuItems: []
+    menuItems: [
+        {
+            id: 1,
+            name: "Rizoto me Perime",
+            description: "Karrot, Kungull, Domate",
+            price: 800,
+            image: ""
+
+        },
+        {
+
+            id: 2,
+            name: "Rizoto me Fruta Deti",
+            description: "Midhje, Karkalec, Kallamar",
+            price: 950,
+            image: ""
+
+        },
+
+
+// Temporary shit:
+
+
+        {
+
+            id: 3,
+            name: "Rizoto me Fruta Deti",
+            description: "Midhje, Karkalec, Kallamar",
+            price: 950,
+            image: ""
+
+        },
+
+        {
+
+            id: 4,
+            name: "Rizoto me Fruta Deti",
+            description: "Midhje, Karkalec, Kallamar",
+            price: 950,
+            image: ""
+
+        },
+
+        {
+
+            id: 5,
+            name: "Rizoto me Fruta Deti",
+            description: "Midhje, Karkalec, Kallamar",
+            price: 950,
+            image: ""
+
+        },
+
+        {
+
+            id: 6,
+            name: "Rizoto me Fruta Deti",
+            description: "Midhje, Karkalec, Kallamar",
+            price: 950,
+            image: ""
+
+        },
+
+        {
+
+            id: 7,
+            name: "Rizoto me Fruta Deti",
+            description: "Midhje, Karkalec, Kallamar",
+            price: 950,
+            image: ""
+
+        },
+
+        {
+
+            id: 8,
+            name: "Rizoto me Fruta Deti",
+            description: "Midhje, Karkalec, Kallamar",
+            price: 950,
+            image: ""
+
+        },
+        {
+
+            id: 9,
+            name: "Rizoto me Fruta Deti",
+            description: "Midhje, Karkalec, Kallamar",
+            price: 950,
+            image: ""
+
+        },
+        {
+
+            id: 10,
+            name: "Rizoto me Fruta Deti",
+            description: "Midhje, Karkalec, Kallamar",
+            price: 950,
+            image: ""
+
+        },
+    ]
 }
