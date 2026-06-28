a4aa5614de10ac248aa74f18deeab22898542bbd
Jurgen Isufaj <notjuri20@gmail.com>
2026-06-28 14:52:38 +0200

    Refactor

---
+++ src/menu-items.js
@@ -0,0 +1,110 @@
+export const menuItems = [
+    {
+        id: 1,
+        name: "Rizoto me Perime",
+        description: "Karrot, Kungull, Domate",
+        price: 800,
+        image: "",
+        categoryIds: [2]
+    },
+    {
+
+        id: 2,
+        name: "Rizoto me Fruta Deti",
+        description: "Midhje, Karkalec, Kallamar",
+        price: 950,
+        image: "",
+        categoryIds: [2]
+
+    },
+
+
+    // Temporary shit:
+
+
+    {
+
+        id: 3,
+        name: "Rizoto me Fruta Deti",
+        description: "Midhje, Karkalec, Kallamar",
+        price: 950,
+        image: "",
+        categoryIds: [3]
+
+    },
+
+    {
+
+        id: 4,
+        name: "Rizoto me Fruta Deti",
+        description: "Midhje, Karkalec, Kallamar",
+        price: 950,
+        image: "",
+        categoryIds: [3]
+
+    },
+
+    {
+
+        id: 5,
+        name: "Rizoto me Fruta Deti",
+        description: "Midhje, Karkalec, Kallamar",
+        price: 950,
+        image: "",
+        categoryIds: [3]
+
+    },
+
+    {
+
+        id: 6,
+        name: "Rizoto me Fruta Deti",
+        description: "Midhje, Karkalec, Kallamar",
+        price: 950,
+        image: "",
+        categoryIds: [3]
+
+    },
+
+    {
+
+        id: 7,
+        name: "Rizoto me Fruta Deti",
+        description: "Midhje, Karkalec, Kallamar",
+        price: 950,
+        image: "",
+        categoryIds: [3]
+
+    },
+
+    {
+
+        id: 8,
+        name: "Rizoto me Fruta Deti",
+        description: "Midhje, Karkalec, Kallamar",
+        price: 950,
+        image: "",
+        categoryIds: [3]
+
+    },
+    {
+
+        id: 9,
+        name: "Rizoto me Fruta Deti",
+        description: "Midhje, Karkalec, Kallamar",
+        price: 950,
+        image: "",
+        categoryIds: [3]
+
+    },
+    {
+
+        id: 10,
+        name: "Rizoto me Fruta Deti",
+        description: "Midhje, Karkalec, Kallamar",
+        price: 950,
+        image: "",
+        categoryIds: [3, 5]
+
+    },
+];
--- src/state.js
+++ src/state.js
@@ -1,3 +1,4 @@
+import { menuItems } from "./menu-items";
 
 const filterCategories = [
     {
@@ -25,116 +26,7 @@ const filterCategories = [
 
 export const state = {
     currentScreen: "MENU",
-    currentFilterId: 3,
+    currentFilterId: 1,
     filterCategories,
-    menuItems: [
-        {
-            id: 1,
-            name: "Rizoto me Perime",
-            description: "Karrot, Kungull, Domate",
-            price: 800,
-            image: "",
-            categoryIds: [2]
-        },
-        {
-
-            id: 2,
-            name: "Rizoto me Fruta Deti",
-            description: "Midhje, Karkalec, Kallamar",
-            price: 950,
-            image: "",
-            categoryIds: [2]
-
-        },
-
-
-// Temporary shit:
-
-
-        {
-
-            id: 3,
-            name: "Rizoto me Fruta Deti",
-            description: "Midhje, Karkalec, Kallamar",
-            price: 950,
-            image: "",
-            categoryIds: [3]
-
-        },
-
-        {
-
-            id: 4,
-            name: "Rizoto me Fruta Deti",
-            description: "Midhje, Karkalec, Kallamar",
-            price: 950,
-            image: "",
-            categoryIds: [3]
-
-        },
-
-        {
-
-            id: 5,
-            name: "Rizoto me Fruta Deti",
-            description: "Midhje, Karkalec, Kallamar",
-            price: 950,
-            image: "",
-            categoryIds: [3]
-
-        },
-
-        {
-
-            id: 6,
-            name: "Rizoto me Fruta Deti",
-            description: "Midhje, Karkalec, Kallamar",
-            price: 950,
-            image: "",
-            categoryIds: [3]
-
-        },
-
-        {
-
-            id: 7,
-            name: "Rizoto me Fruta Deti",
-            description: "Midhje, Karkalec, Kallamar",
-            price: 950,
-            image: "",
-            categoryIds: [3]
-
-        },
-
-        {
-
-            id: 8,
-            name: "Rizoto me Fruta Deti",
-            description: "Midhje, Karkalec, Kallamar",
-            price: 950,
-            image: "",
-            categoryIds: [3]
-
-        },
-        {
-
-            id: 9,
-            name: "Rizoto me Fruta Deti",
-            description: "Midhje, Karkalec, Kallamar",
-            price: 950,
-            image: "",
-            categoryIds: [3]
-
-        },
-        {
-
-            id: 10,
-            name: "Rizoto me Fruta Deti",
-            description: "Midhje, Karkalec, Kallamar",
-            price: 950,
-            image: "",
-            categoryIds: [3, 5]
-
-        },
-    ]
+    menuItems
 }
