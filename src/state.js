250d0d8705ce8f7177d69470b2418cdde736f95e
Jurgen Isufaj <notjuri20@gmail.com>
2026-07-23 15:08:42 +0200

    Upd state

--- src/state.js
+++ src/state.js
@@ -2,11 +2,10 @@ import { filterCategories } from "./data/filter-categories";
 import { menuItems } from "./data/menu-items";
 
 export const state = {
-    currentScreen: "ORDER", // "MENU" | "ORDER" | "CONFIRM"
+    currentScreen: "MENU", // "MENU" | "ORDER" | "CONFIRM"
     currentFilterId: 1,
     filterCategories,
     menuItems,
     orderedItems: {
-        2: 1
     }, // Keys are menu item ids, values are amount 
 }
